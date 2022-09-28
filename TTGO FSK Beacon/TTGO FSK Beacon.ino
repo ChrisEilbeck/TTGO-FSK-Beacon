4db3d642773218c662b3ee4d674ecb8064e48942
Chris Eilbeck <chris@hyperspace.org.uk>
2022-09-28 18:50:48 +0100

    This will read back the version of the chip correctly from a TTGO Lora32 v1.6 using the LoRaPlusFSK library.

--- TTGO FSK Beacon/TTGO FSK Beacon.ino
+++ TTGO FSK Beacon/TTGO FSK Beacon.ino
@@ -1,14 +1,31 @@
 
+#include "LoRaPlusFSK.h"
+
 #define GREEN_LED	25
 
 void setup(void)
 {
+	Serial.begin(115200);
+	
+	while(!Serial);
+	
+	LoRaPlusFSK.setPins(18,23,26);
+	LoRaPlusFSK.begin(433920000);
+	
 	pinMode(GREEN_LED,OUTPUT);
 	digitalWrite(GREEN_LED,LOW);
 }
 
 void loop(void)
 {
+	uint8_t version=LoRaPlusFSK.readRegister(REG_VERSION);
+	Serial.print("LoRa chip version: ");
+	Serial.println(version);
+	
+	digitalWrite(GREEN_LED,HIGH);
+	delay(50);
+	digitalWrite(GREEN_LED,LOW);
+	delay(50);
 	digitalWrite(GREEN_LED,HIGH);
 	delay(50);
 	digitalWrite(GREEN_LED,LOW);
