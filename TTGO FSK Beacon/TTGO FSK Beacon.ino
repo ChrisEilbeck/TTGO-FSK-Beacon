84a931f72876b603fd0d7c9e65586721858e5d47
Chris Eilbeck <chris@hyperspace.org.uk>
2022-10-01 15:32:34 +0100

    This might work, got no hardware with me here to test it as yet.

--- TTGO FSK Beacon/TTGO FSK Beacon.ino
+++ TTGO FSK Beacon/TTGO FSK Beacon.ino
@@ -18,9 +18,23 @@ void setup(void)
 
 void loop(void)
 {
+#if 0
 	uint8_t version=LoRaPlusFSK.readRegister(REG_VERSION);
 	Serial.print("LoRa chip version: ");
 	Serial.println(version);
+#endif
+#if 1
+	LoRaPlusFSK.setFSKMode();
+	LoRaPlusFSK.setTxPower(5);
+	LoRaPlusFSK.setFSKBitRate(1000);
+	
+	uint8_t TxPacket[]={0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa};
+	uint16_t TxPacketLength=32;
+	
+	LoRaPlusFSK.beginPacket(false);
+	LoRaPlusFSK.write(TxPacket,TxPacketLength);
+	LoRaPlusFSK.endPacket(false);
+#endif
 	
 	digitalWrite(GREEN_LED,HIGH);
 	delay(50);
