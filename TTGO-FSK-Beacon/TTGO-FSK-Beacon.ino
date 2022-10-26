180d4a1c98e8f3013e4c6612784fc6a22fd86884
Chris Eilbeck <chris@hyperspace.org.uk>
2022-10-26 22:55:21 +0100

    Steps towards supporting other frequency bands.  Not complete.   Not tested.  Don't use!

--- TTGO-FSK-Beacon/TTGO-FSK-Beacon.ino
+++ TTGO-FSK-Beacon/TTGO-FSK-Beacon.ino
@@ -1,5 +1,4 @@
 
-#define SLEEP_DURATION			2000000
 #define SLEEP_MODE_OPERATION	1
 
 #ifdef ARDUINO_TBeam
@@ -47,51 +46,103 @@ static const unsigned char PROGMEM logo_bmp[] =
 	B00000000, B00110000
 };
 
-#define DISPLAYUPDATEPERIOD	250
+#define DISPLAY_UPDATE_PERIOD	250
 
 // pin assignments for various boards we support
 
 #ifdef ARDUINO_TBeam
-	#define LoRa_NSS	18
-	#define LoRa_DIO0	26
-	#define LoRa_RESET	14
-	#define LoRa_DIO1	-1
+	#define LoRa_NSS				18
+	#define LoRa_DIO0				26
+	#define LoRa_RESET				14
+	#define LoRa_DIO1				-1
+	
+	#define BATTERY_VOLTAGE_PIN		-1
+	#define BATTERY_CAL_VALUE		1.734
+	
+	#define USER_BUTTON				-1
 #endif
 #ifdef ARDUINO_TTGO_LoRa32_v21new
-	#define LoRa_NSS	18
-	#define LoRa_DIO0	26
-	#define LoRa_RESET	23
-	#define LoRa_DIO1	-1
+	#define LoRa_NSS				18
+	#define LoRa_DIO0				26
+	#define LoRa_RESET				23
+	#define LoRa_DIO1				-1
+	
+	#define BATTERY_VOLTAGE_PIN		35
+	#define BATTERY_CAL_VALUE		1.734
+	
+	#define USER_BUTTON				-1
 #endif
 #ifdef ARDUINO_TTGO_LoRa32_V1
-	#define LoRa_NSS	5
-	#define LoRa_DIO0	26
-	#define LoRa_RESET	4
-	#define LoRa_DIO1	-1
+	#define LoRa_NSS				5
+	#define LoRa_DIO0				26
+	#define LoRa_RESET				4
+	#define LoRa_DIO1				-1
+	
+	#define BATTERY_VOLTAGE_PIN		35
+	#define BATTERY_CAL_VALUE		1.734
+	
+	#define USER_BUTTON				-1
 #endif
 #ifdef ADAFRUIT_FEATHER_M0
-	#define LoRa_NSS	8
-	#define LoRa_DIO0	3
-	#define LoRa_RESET	4
-	#define LoRa_DIO1	-1
+	#define LoRa_NSS				8
+	#define LoRa_DIO0				3
+	#define LoRa_RESET				4
+	#define LoRa_DIO1				-1
+	
+	#define BATTERY_VOLTAGE_PIN		-1
+	#define BATTERY_CAL_VALUE		1.734
+	
+	#define USER_BUTTON				-1
 #endif
 
-#define BATTERYVOLTAGEPIN	35
-#define BATTERYCALVALUE		1.734
+#define MIN_FREQUENCY4				430.000
+#define DEFAULT_FREQUENCY4			433.920
+#define MAX_FREQUENCY4				440.000
 
-#define EEPROM_SIZE 		8
+#define MIN_FREQUENCY8				868.000
+#define DEFAULT_FREQUENCY8			869.500
+#define MAX_FREQUENCY8				870.000
 
-#define MIN_FREQUENCY		430.000
-#define DEFAULT_FREQUENCY	433.920
-#define MAX_FREQUENCY		440.000
+#define MIN_FREQUENCY9				902.000
+#define DEFAULT_FREQUENCY9			915.000
+#define MAX_FREQUENCY9				930.000
 
-#define FINE_STEP			0.001
-#define COARSE_STEP			0.005
-#define VERY_COARSE_STEP	0.100
+#define MIN_POWER_LEVEL				2
+#define DEFAULT_POWER_LEVEL			10
+#define MAX_POWER_LEVEL				17
+
+#define FINE_STEP					0.001
+#define COARSE_STEP					0.005
+#define VERY_COARSE_STEP			0.100
+
+#define EEPROM_SIZE 				8
+
+#define EEPROM_F1_ADDRESS			0x00
+#define EEPROM_F2_ADDRESS			0x01
+#define EEPROM_F3_ADDRESS			0x02
+#define EEPROM_F4_ADDRESS			0x03
+
+#define EEPROM_POWER_LEVEL_ADDRESS	0x04
 
 SX1278 radio=new Module(LoRa_NSS,LoRa_DIO0,LoRa_RESET,LoRa_DIO1);
-float frequency=DEFAULT_FREQUENCY;
+
+// set up some defaults in case we can't read the programmed values from the eeprom
+
+int frequency_band=4;
+float frequency=DEFAULT_FREQUENCY4;
+
+int old_frequency_band=4;
+float old_frequency=DEFAULT_FREQUENCY4;
+
+float min_frequency=MIN_FREQUENCY4;
+float default_frequency=DEFAULT_FREQUENCY4;
+float max_frequency=MAX_FREQUENCY4;
+
+// time between transmissions
+
 int txperiod=2000;
+
+int power_level=DEFAULT_POWER_LEVEL;
 
 void setledon(void)
 {
@@ -117,32 +168,41 @@ float readbatteryvoltage(void)
 	float batvolt=axp.getBattVoltage();
 	return(batvolt);
 #else
-	int adcvalue=analogRead(BATTERYVOLTAGEPIN);
-	return(BATTERYCALVALUE*(float)adcvalue);
+	int adcvalue=analogRead(BATTERY_VOLTAGE_PIN);
+	return(BATTERY_CAL_VALUE*(float)adcvalue);
 #endif
 }
 
 void update_frequency(float freq)
 {
-	int state=radio.setFrequency(freq);
-	if(state==RADIOLIB_ERR_NONE)	{	Serial.println("Success ...");	}	else	{	Serial.print(F("failed, code "));	Serial.println(state);	}
-	
 	Serial.print("Frequency: ");
 	Serial.printf("%03.3f MHz",freq);
 	Serial.println();
 	
-#ifndef ADAFRUIT_FEATHER_M0
-	// store the frequency to eeprom
+	int state=radio.setFrequency(freq);
+	if(state==RADIOLIB_ERR_NONE)	{	Serial.println("Success ...");	}	else	{	Serial.print(F("failed, code "));	Serial.println(state);	}
 	
+#ifndef ADAFRUIT_FEATHER_M0
 	uint8_t buffer[4];
 	memcpy(buffer,(uint8_t *)&freq,4);
 	
-    EEPROM.write(0,buffer[0]);
-    EEPROM.write(1,buffer[1]);
-    EEPROM.write(2,buffer[2]);
-    EEPROM.write(3,buffer[3]);
+	EEPROM.write(EEPROM_F1_ADDRESS,buffer[0]);	EEPROM.write(EEPROM_F2_ADDRESS,buffer[1]);	EEPROM.write(EEPROM_F3_ADDRESS,buffer[2]);	EEPROM.write(EEPROM_F4_ADDRESS,buffer[3]);
+	EEPROM.commit();
+#endif
+}
+
+void update_power_level(int power_level)
+{
+	Serial.print("Setting power level to ");
+	Serial.print(power_level);
+	Serial.println(" dBm");
 	
-    EEPROM.commit();
+	int state=radio.setOutputPower((float)power_level);
+	if(state==RADIOLIB_ERR_NONE)	{	Serial.println("Success ...");	}	else	{	Serial.print(F("failed, code "));	Serial.println(state);	}
+	
+#ifndef ADAFRUIT_FEATHER_M0
+	EEPROM.write(EEPROM_POWER_LEVEL_ADDRESS,power_level);
+	EEPROM.commit();
 #endif
 }
 
@@ -151,9 +211,16 @@ void displaymenu(void)
 	Serial.print("\nTTGO-FSK-BEACON\r\n===============\r\n\n");
 	Serial.print("Command menu\r\n------------\r\n\n");
 	Serial.print("\tt\t-\tTransmit a burst\r\n");
-	Serial.print("\t0\t-\tRestore default settings\r\n");
+	Serial.print("\t0\t-\tRestore default frequency\r\n");
 	Serial.print("\t+/u/U\t-\tIncrease frequency in fine, coarse or very coarse steps\r\n");
 	Serial.print("\t-/d/D\t-\tDecrease frequency in fine, coarse or very coarse steps\r\n");
+	Serial.print("\t4-\t-Operate in 433MHz band\r\n");
+	Serial.print("\t8-\t-Operate in 868MHz band\r\n");
+	Serial.print("\t9-\t-Operate in 915MHz band\r\n");
+	Serial.print("\to\t-\tRestore the default power level\r\n");
+	Serial.print("\tp\t-\tDecrease the power level\r\n");
+	Serial.print("\tP\t-\tIncrease the power level\r\n");
+	Serial.print("\tb\t-\tDisplay the battery voltage\r\n");
 	Serial.print("\tl/L\t-\tGreen LED on/off\r\n");
 	Serial.print("\tr\t-\tEnter run mode\r\n");
 	Serial.print("\tc\t-\tStay in config mode, don't exit to run mode\r\n");
@@ -236,10 +303,10 @@ void setup(void)
 	uint8_t buffer[4];
 	float freqval;
 	
-	buffer[0]=EEPROM.read(0);
-	buffer[1]=EEPROM.read(1);
-	buffer[2]=EEPROM.read(2);
-	buffer[3]=EEPROM.read(3);
+	buffer[0]=EEPROM.read(EEPROM_F1_ADDRESS);
+	buffer[1]=EEPROM.read(EEPROM_F2_ADDRESS);
+	buffer[2]=EEPROM.read(EEPROM_F3_ADDRESS);
+	buffer[3]=EEPROM.read(EEPROM_F4_ADDRESS);
 	
 #if 0
 	Serial.println(buffer[0],HEX);
@@ -250,19 +317,12 @@ void setup(void)
 	
 	memcpy((uint8_t *)&freqval,buffer,4);
 	
-	if(		(freqval<MIN_FREQUENCY)
-		||	(freqval>MAX_FREQUENCY)
-		||	isnan(freqval)				)
+	if(isnan(freqval))
 	{
 		Serial.println("Stored frequency is invalid, subsituting the default");
-		frequency=DEFAULT_FREQUENCY;
-		update_frequency(frequency);
-	}
-	else
-	{
-		frequency=freqval;
-		Serial.print("Frequency: ");
-		Serial.printf("%03.3f MHz",frequency);
+		frequency=DEFAULT_FREQUENCY4;
+		min_frequency=MIN_FREQUENCY4;
+		max_frequency=MAX_FREQUENCY4;
 		Serial.println();
 	}
 #else
@@ -271,6 +331,16 @@ void setup(void)
 	Serial.printf("%03.3f MHz",frequency);
 	Serial.println();
 #endif
+	
+#ifndef ADAFRUIT_FEATHER_M0
+	power_level=EEPROM.read(4);
+#else
+	power_level=DEFAULT_POWER_LEVEL;
+#endif
+	
+	Serial.print("Setting power level to ");
+	Serial.print(power_level);
+	Serial.println(" dBm");
 	
 	// the following settings can also
 	// be modified at run-time
@@ -283,10 +353,7 @@ void setup(void)
 	state=radio.setFrequencyDeviation(10.0);
 //	if(state==RADIOLIB_ERR_NONE)	{	Serial.println("Success ...");	}	else	{	Serial.print(F("failed, code "));	Serial.println(state);	}
 	
-	state=radio.setRxBandwidth(250.0);
-//	if(state==RADIOLIB_ERR_NONE)	{	Serial.println("Success ...");	}	else	{	Serial.print(F("failed, code "));	Serial.println(state);	}
-	
-	state=radio.setOutputPower(10.0);
+	state=radio.setOutputPower((float)power_level);
 //	if(state==RADIOLIB_ERR_NONE)	{	Serial.println("Success ...");	}	else	{	Serial.print(F("failed, code "));	Serial.println(state);	}
 	
 	state=radio.setCurrentLimit(100);
@@ -301,6 +368,9 @@ void setup(void)
 	uint8_t syncWord[]={	0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa,0xaa	};
 	
 	state=radio.setSyncWord(syncWord, 8);
+//	if(state==RADIOLIB_ERR_NONE)	{	Serial.println("Success ...");	}	else	{	Serial.print(F("failed, code "));	Serial.println(state);	}
+	
+	state=radio.setRxBandwidth(50.0);
 //	if(state==RADIOLIB_ERR_NONE)	{	Serial.println("Success ...");	}	else	{	Serial.print(F("failed, code "));	Serial.println(state);	}
 	
 #ifndef ARDUINO_TBeam
@@ -321,7 +391,7 @@ void loop(void)
 	{
 		static int displaylastupdate=0;
 		
-		if(millis()>(displaylastupdate+DISPLAYUPDATEPERIOD))
+		if(millis()>(displaylastupdate+DISPLAY_UPDATE_PERIOD))
 		{
 			float batvolt=readbatteryvoltage();
 			
@@ -397,41 +467,87 @@ void loop(void)
 #ifndef ADAFRUIT_FEATHER_M0
 				// frequency adjustment commands
 				
-				case '0':	frequency=DEFAULT_FREQUENCY;
+				case '0':	frequency=default_frequency;
 							update_frequency(frequency);
 							break;
 				
-				case '+':	if(frequency<MAX_FREQUENCY)	frequency+=FINE_STEP;
-							if(frequency>MAX_FREQUENCY)	frequency=MAX_FREQUENCY;
+				case '+':	if(frequency<max_frequency)	frequency+=FINE_STEP;
+							if(frequency>max_frequency)	frequency=max_frequency;
 							update_frequency(frequency);
 							break;
 				
-				case '-':	if(frequency>MIN_FREQUENCY)	frequency-=FINE_STEP;
-							if(frequency<MIN_FREQUENCY)	frequency=MIN_FREQUENCY;
+				case '-':	if(frequency>min_frequency)	frequency-=FINE_STEP;
+							if(frequency<min_frequency)	frequency=min_frequency;
 							update_frequency(frequency);
 							break;
 				
-				case 'u':	if(frequency<MAX_FREQUENCY)	frequency+=COARSE_STEP;
-							if(frequency>MAX_FREQUENCY)	frequency=MAX_FREQUENCY;
+				case 'u':	if(frequency<max_frequency)	frequency+=COARSE_STEP;
+							if(frequency>max_frequency)	frequency=max_frequency;
 							update_frequency(frequency);
 							break;
 							
-				case 'd':	if(frequency>MIN_FREQUENCY)	frequency-=COARSE_STEP;
-							if(frequency<MIN_FREQUENCY)	frequency=MIN_FREQUENCY;
+				case 'd':	if(frequency>min_frequency)	frequency-=COARSE_STEP;
+							if(frequency<min_frequency)	frequency=min_frequency;
 							update_frequency(frequency);
 							break;
 							
-				case 'U':	if(frequency<MAX_FREQUENCY)	frequency+=VERY_COARSE_STEP;
-							if(frequency>MAX_FREQUENCY)	frequency=MAX_FREQUENCY;
+				case 'U':	if(frequency<max_frequency)	frequency+=VERY_COARSE_STEP;
+							if(frequency>max_frequency)	frequency=max_frequency;
 							update_frequency(frequency);
 							break;
 							
-				case 'D':	if(frequency>MIN_FREQUENCY)	frequency-=VERY_COARSE_STEP;
-							if(frequency<MIN_FREQUENCY)	frequency=MIN_FREQUENCY;
+				case 'D':	if(frequency>min_frequency)	frequency-=VERY_COARSE_STEP;
+							if(frequency<min_frequency)	frequency=min_frequency;
 							update_frequency(frequency);
 							break;
 #endif
+				case '4':	Serial.println("Selecting operation in the 433MHz band");
+							frequency_band=4;
+							frequency=DEFAULT_FREQUENCY4;
+							min_frequency=MIN_FREQUENCY4;
+							max_frequency=MAX_FREQUENCY4;
+							update_frequency(frequency);
+							break;
 				
+				case '8':	Serial.println("Selecting operation in the 869MHz band");
+							frequency_band=8;
+							frequency=DEFAULT_FREQUENCY8;
+							min_frequency=MIN_FREQUENCY8;
+							max_frequency=MAX_FREQUENCY8;
+							update_frequency(frequency);
+							break;
+				
+				case '9':	Serial.println("Selecting operation in the 915MHz band");
+							frequency_band=9;
+							frequency=DEFAULT_FREQUENCY9;
+							min_frequency=MIN_FREQUENCY9;
+							max_frequency=MAX_FREQUENCY9;
+							update_frequency(frequency);
+							break;
+				
+				// power adjustment commands
+				
+				case 'p':	if(power_level>MIN_POWER_LEVEL)
+							{
+								power_level--;
+								update_power_level(power_level);
+							}
+							
+							break;
+							
+				case 'P':	if(power_level<MAX_POWER_LEVEL)
+							{
+								power_level++;
+								update_power_level(power_level);
+							}
+							
+							break;
+				
+				case 'o':	power_level=DEFAULT_POWER_LEVEL;
+							update_power_level(power_level);
+							
+							break;
+							
 				// misc features
 				
 				case 'b':	Serial.print("Battery voltage: ");
@@ -512,7 +628,7 @@ void loop(void)
 		}
 #endif
 #if SLEEP_MODE_OPERATION
-		esp_sleep_enable_timer_wakeup(SLEEP_DURATION);
+		esp_sleep_enable_timer_wakeup(txperiod*1000);
 		esp_light_sleep_start();
 #endif
 	}
